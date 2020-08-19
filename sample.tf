594f5475f12dc6ac9c91d4819891be6aff27c9db
Dhairyasheel <sutardhiraj98@gmail.com>
2020-08-19 17:28:58 +0530

    startup script for ec2

--- sample.tf
+++ sample.tf
@@ -8,6 +8,7 @@ resource "aws_instance" "instance_20942_pub" {
     instance_type          = "t2.micro"
     subnet_id              = "subnet-0783890f7bd7f6efb"
     vpc_security_group_ids = ["sg-0def65b68a4abd34d"]
+    user_data              = "${file("startup_script.sh")}"
     key_name               = "dhairyasheel-20942-key-pair"
     associate_public_ip_address = "true"
     tags {
@@ -15,3 +16,21 @@ resource "aws_instance" "instance_20942_pub" {
     }
 }
 
+resource "aws_ami_from_instance" "ami_instance" {
+    name               = "ami-20942"
+    source_instance_id = "${aws_instance.instance_20942_pub.id}"
+    tags {
+        Name = "ami-20942"
+    }
+}
+
+resource "aws_instance" "instance_20942_pri" {
+    ami                    = "${aws_ami_from_instance.ami_instance.id}"
+    instance_type          = "t2.micro"
+    subnet_id              = "subnet-040b66e757e6ca460"
+    vpc_security_group_ids = ["sg-0def65b68a4abd34d"]
+    key_name               = "dhairyasheel-20942-key-pair"
+    tags {
+        Name = "20942-pvt-instance"
+    }
+}
