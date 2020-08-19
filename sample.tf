5a8e9d715468bfeeef8819afc62617aee76c027c
Dhairyasheel <sutardhiraj98@gmail.com>
2020-08-19 17:54:18 +0530

    user data settings

--- sample.tf
+++ sample.tf
@@ -3,17 +3,16 @@ provider "aws" {
     region = "us-east-1"
 }
 
-data "template_file" "user_data" {
-    template = "${file("startup_script.sh")}"
-}
-
-
 resource "aws_instance" "instance_20942_pub" {
     ami                    = "ami-0bcc094591f354be2"
     instance_type          = "t2.micro"
     subnet_id              = "subnet-0783890f7bd7f6efb"
     vpc_security_group_ids = ["sg-0def65b68a4abd34d"]
-    user_data              = "${data.template_file.user_data.rendered}"
+    user_data              = << EOF
+                            #! /bin/bash
+                            sudo apt-get update -y
+                            sudo apt-get install mysql-client -y
+                            EOF
     key_name               = "dhairyasheel-20942-key-pair"
     associate_public_ip_address = "true"
     tags {
@@ -21,10 +20,3 @@ resource "aws_instance" "instance_20942_pub" {
     }
 }
 
-resource "aws_ami_from_instance" "ami_instance" {
-    name               = "ami-20942"
-    source_instance_id = "${aws_instance.instance_20942_pub.id}"
-    tags {
-        Name = "ami-20942"
-    }
-}
