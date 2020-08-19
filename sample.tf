d5ab1d1bdf7a09b16cd1d8e54050cb9546931820
Dhairyasheel <sutardhiraj98@gmail.com>
2020-08-19 17:56:50 +0530

    user data

--- sample.tf
+++ sample.tf
@@ -8,11 +8,9 @@ resource "aws_instance" "instance_20942_pub" {
     instance_type          = "t2.micro"
     subnet_id              = "subnet-0783890f7bd7f6efb"
     vpc_security_group_ids = ["sg-0def65b68a4abd34d"]
-    user_data              = << EOF
-                            #! /bin/bash
+    user_data              = "#! /bin/bash
                             sudo apt-get update -y
-                            sudo apt-get install mysql-client -y
-                            EOF
+                            sudo apt-get install mysql-client -y"
     key_name               = "dhairyasheel-20942-key-pair"
     associate_public_ip_address = "true"
     tags {
