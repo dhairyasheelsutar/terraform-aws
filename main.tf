6c87ba555c5bc408cbbcd1fe89cd42b1ca09614f
Dhairyasheel <sutardhiraj98@gmail.com>
2020-08-20 19:20:01 +0530

    updated main with bash

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 resource "null_resource" "example2" {
-  provisioner "local-exec" {
-    command = <<-EOF
-                #!/bin/bash
-                aws s3 cp s3://bucket-20942/db_credentials.sh .
-                chmod +x db_credentials.sh
-                source db_credentials.sh
-                EOF
-  }
+    provisioner "local-exec" {
+        command = ["/bin/bash", "-c", "setup.sh"]
+    }
 }
---
+++ setup.sh
@@ -0,0 +1,3 @@
+aws s3 cp s3://bucket-20942/db_credentials.sh .
+chmod +x db_credentials.sh
+source db_credentials.sh
